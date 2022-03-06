f1eeabc7990c9c7e0c02d23915868813428f45a1
khairil <80386217+khairil8@users.noreply.github.com>
2022-03-06 16:16:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['601123550412', '60109102984', '601123550412'] // Put your number here
+global.mods = [601123550412] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -28,7 +28,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_author = 'Khairil'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
